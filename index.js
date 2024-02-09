5525ff5deab626cc2032cace31c50646aa99ccc4
hyadess <sayemshahadsoummo@gmail.com>
2024-02-09 15:28:47 +0600

    change

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express=require('express');
 const app=express();
 const port=8000;
-const pool=require('./db');
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 });
