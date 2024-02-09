49d663efdc35d90c39b036ef4a060a899daa8584
hyadess <sayemshahadsoummo@gmail.com>
2024-02-09 16:14:54 +0600

    fr

--- database/setup.sh
+++
@@ -1,4 +0,0 @@
-sudo -u postgres psql -c "DROP DATABASE codesamurai;"
-sudo -u postgres psql -c "CREATE DATABASE codesamurai;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE codesamurai TO postgres;"
-sudo -u postgres psql -d codesamurai -a -f init.sql
