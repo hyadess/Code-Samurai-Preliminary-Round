b9cb3104bc2610de75a789cdce1315899de6cbb2
mahirlabibdihan <mahirlabibdihan@gmail.com>
2024-02-09 16:05:42 +0600

    Update setup.sh script to use sudo for PostgreSQL commands

--- setup.sh
+++ setup.sh
@@ -1,1 +1,3 @@
-psql -c "ALTER USER postgres WITH PASSWORD 'root';" && psql -c "CREATE DATABASE codesamurai;" && psql -c "GRANT ALL PRIVILEGES ON DATABASE codesamurai TO postgres;"
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'root';"
+sudo -u postgres psql -c "CREATE DATABASE codesamurai;"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE codesamurai TO postgres;"
